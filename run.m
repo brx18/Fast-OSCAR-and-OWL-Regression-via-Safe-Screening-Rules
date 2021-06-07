b306c671440bb24f0df3180a68473d26d7e216dd
brx18 <34207191+brx18@users.noreply.github.com>
2021-06-07 00:47:40 -0400

    Update run.m

--- run.m
+++ run.m
@@ -1,3 +1,8 @@
+% This file is a demo of safe screening for OWL regression for our ICML paper: Fast OSCAR and OWL Regression via Safe Screening Rules at http://proceedings.mlr.press/v119/bao20b.html
+% APGD: An accelerated proximal gradient method (FISTA-type) in \cite{bogdan2015slope}
+% APGDScreen: APGD method with our proposed screening;
+% SPGD: An proximal stochastic variance-reduced method (ProxSVRG) in \cite{xiao2014proximal}.;
+% SPGDScreen: An ProxSVRG method with our proposed screening.
 
 clear all;
 
@@ -6,11 +11,13 @@ path_func1 = 'solver/method';
 addpath(path_func0);
 addpath(path_func1);
 
+% generate the data
 n = 1000;
 d = 10000;
 A  = randn(n,d);
 b  = randn(n,1);
 
+% generate the regularization parameters
 lambda1_max = max(A'*b);
 p = 0.2;
 lambda1 = p*lambda1_max;
@@ -19,6 +26,7 @@ for i = 1:d
     lambda(i) = lambda1 + (d-i)*lambda2;
 end
 
+% methods
 [x1,info1] = APGD(A,b,lambda);
 [x2,info2] = APGDScreen(A,b,lambda);
 [x3,info3] = SPGD(A,b,lambda);
